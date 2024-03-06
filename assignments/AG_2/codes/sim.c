1036d7638e8cd98d5e8b525fa1015397b3e727eb
Varunryaliiith <ee23btech11220@iith.ac.in>
2024-03-06 07:57:40 +0530

    update

--- assignments/AG_2/codes/sim.c
+++ assignments/AG_2/codes/sim.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+ double x=1;
+ double result=0;
+ 
+ double func(double x) {
+   
+   if(x<10000000000000000){
+       
+       result=(x*(x+1))/(2*x*x);
+       printf("For x=%.0f,the value is %.15f\n",x,result);
+      func(10*x);
+   }
+     
+ }
+ 
+void main() {
+    
+func(x);
 
-int main() {
-    // Define a variable for x and the result
-    double x;
-    double result;
-
-    printf("This program calculates the value of x(x+1)/(2x^2) for increasing values of x and shows how it tends to 1/2 as x tends to infinity.\n\n");
-
-    // Loop through a range of values for x
-    for (x = 1; x <= 1000000000000000; x *= 10) { // Increasing x by a factor of 10 each time
-        // Calculate the value of the expression
-        result = (x * (x + 1)) / (2 * x * x);
-
-        // Print the result
-        printf("For x = %.0f, x(x+1)/(2x^2) = %.15f\n", x, result);
-    }
-
-    return 0;
 }
 
